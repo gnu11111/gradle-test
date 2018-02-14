a9e9493a051909c0322719c68374438911a1c401
gnu <gnu@gnu.at>
2018-02-14 16:06:42 +0100

    simple gradle project

---
+++ src/main/java/at/gnu/test/gradle/GradleTest.java
@@ -0,0 +1,10 @@
+package at.gnu.test.gradle;
+
+
+public class GradleTest
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Gradle!");
+    }
+}
---
+++ src/test/java/at/gnu/test/gradle/TestGradleTest.java
@@ -0,0 +1,13 @@
+package at.gnu.test.gradle;
+
+import org.junit.Test;
+
+
+public class TestGradleTest
+{
+    @Test
+    public void testMain()
+    {
+        GradleTest.main(null);
+    }
+}
